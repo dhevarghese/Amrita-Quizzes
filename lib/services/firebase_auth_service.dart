dc169f86e5d7cd7e13cb4e682169c6b9b6421833
ColdCoffee21 <dheerajvarghese@gmail.com>
2021-04-03 13:06:50 +0530

    Implemented Firebase auth class with all required functionalities.
    Signed-off-by: ColdCoffee21 <dheerajvarghese@gmail.com>

---
+++ lib/services/firebase_auth_service.dart
@@ -0,0 +1,97 @@
+import 'package:amrita_quizzes/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthService implements AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  MyAppUser _userFromFirebase(User user) {
+    if (user == null) {
+      return null;
+    }
+    return MyAppUser(
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoUrl: user.photoURL,
+    );
+  }
+
+  @override
+  Stream<MyAppUser> get onAuthStateChanged {
+    return _firebaseAuth.authStateChanges().map(_userFromFirebase);
+  }
+
+  @override
+  Future<MyAppUser> signInWithEmailAndPassword(
+      String email, String password) async {
+    final UserCredential userCredential =
+        await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return _userFromFirebase(userCredential.user);
+  }
+
+  @override
+  Future<MyAppUser> createUserWithEmailAndPassword(
+      String email, String password) async {
+    final UserCredential userCredential = await _firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return _userFromFirebase(userCredential.user);
+  }
+
+  @override
+  Future<void> sendPasswordResetEmail(String email) async {
+    await _firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+  @override
+  Future<MyAppUser> signInWithEmailAndLink({String email, String link}) async {
+    final UserCredential userCredential =
+        await _firebaseAuth.signInWithEmailLink(email: email, emailLink: link);
+    return _userFromFirebase(userCredential.user);
+  }
+
+  @override
+  Future<MyAppUser> signInWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
+
+    if (googleUser != null) {
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+        final UserCredential userCredential = await _firebaseAuth
+            .signInWithCredential(GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        ));
+        return _userFromFirebase(userCredential.user);
+      } else {
+        throw PlatformException(
+            code: 'ERROR_MISSING_GOOGLE_AUTH_TOKEN',
+            message: 'Missing Google Auth Token');
+      }
+    } else {
+      throw PlatformException(
+          code: 'ERROR_ABORTED_BY_USER', message: 'Sign in aborted by user');
+    }
+  }
+
+  @override
+  Future<MyAppUser> currentUser() async {
+    return _userFromFirebase(_firebaseAuth.currentUser);
+  }
+
+  @override
+  Future<void> signOut() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
+    return _firebaseAuth.signOut();
+  }
+
+  @override
+  void dispose() {}
+}

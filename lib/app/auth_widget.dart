4c503915545d98c7ee63eb1b59372ed476aa3e58
ColdCoffee21 <dheerajvarghese@gmail.com>
2021-04-03 14:48:20 +0530

    Build the logged in/logged out UI based on user snapshot
    Signed-off-by: ColdCoffee21 <dheerajvarghese@gmail.com>

---
+++ lib/app/auth_widget.dart
@@ -0,0 +1,25 @@
+import 'package:amrita_quizzes/app/home_page.dart';
+import 'package:amrita_quizzes/app/sign_in/sign_in_page.dart';
+import 'package:amrita_quizzes/services/auth_service.dart';
+import 'package:flutter/material.dart';
+
+/// Builds the signed-in or non signed-in UI, depending on the user snapshot.
+/// This widget should be below the [MaterialApp].
+/// An [AuthWidgetBuilder] ancestor is required for this widget to work.
+/// Note: this class used to be called [LandingPage].
+class AuthWidget extends StatelessWidget {
+  const AuthWidget({Key key, @required this.userSnapshot}) : super(key: key);
+  final AsyncSnapshot<MyAppUser> userSnapshot;
+
+  @override
+  Widget build(BuildContext context) {
+    if (userSnapshot.connectionState == ConnectionState.active) {
+      return userSnapshot.hasData ? HomePage() : SignInPageBuilder();
+    }
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}

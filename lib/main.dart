f6ab6adf5aa110f99904fd230ce0b043f157124b
ColdCoffee21 <dheerajvarghese@gmail.com>
2021-04-03 17:16:42 +0530

    Rewrote to switch from mock auth to firebase authentication service
    Signed-off-by: ColdCoffee21 <dheerajvarghese@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,54 @@
-import 'package:amrita_quizzes/homePage.dart';
-import 'package:amrita_quizzes/loginPage.dart';
+import 'package:amrita_quizzes/app/auth_widget.dart';
+import 'package:amrita_quizzes/app/auth_widget_builder.dart';
+import 'package:amrita_quizzes/services/auth_service.dart';
+import 'package:amrita_quizzes/services/auth_service_adapter.dart';
+import 'package:amrita_quizzes/services/email_secure_store.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final routes = <String, WidgetBuilder>{
-    LoginPage.tag: (context) => LoginPage(),
-    HomePage.tag: (context) => HomePage(),
-    //SignUpPage.tag: (context) => SignUpPage()
-  };
+  const MyApp(
+      {Key key,
+        this.initialAuthServiceType = AuthServiceType.firebase})
+      : super(key: key);
+
+  final AuthServiceType initialAuthServiceType;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Amrita Quizzes',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        fontFamily: 'Nunito',
+    return MultiProvider(
+      providers: [
+        Provider<AuthService>(
+            create: (_) => AuthServiceAdapter(
+                initialAuthServiceType: initialAuthServiceType),
+            dispose: (_, AuthService authService) => authService.dispose()),
+        Provider<EmailSecureStore>(
+          create: (_) => EmailSecureStore(
+            flutterSecureStorage: FlutterSecureStorage(),
+          ),
+        ),
+      ],
+      child: AuthWidgetBuilder(
+        builder: (BuildContext context, AsyncSnapshot<MyAppUser> userSnapshot) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.lightBlue,
+              fontFamily: 'Nunito',
+            ),
+            home: AuthWidget(userSnapshot: userSnapshot,
+            ),
+          );
+        },
       ),
-      home: LoginPage(),
-      routes: routes,
     );
   }
 }

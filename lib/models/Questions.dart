507cbcfbdc7403a5c83c806c944d6031d2249fd0
kishoresara <kishoresaravanan91@gmail.com>
2021-04-04 21:27:12 +0530

    added custom models for quiz info and the question info
    Signed-off-by: Kishore Saravanan <kidhoresaravanan91@gmail.com>

---
+++ lib/models/Questions.dart
@@ -0,0 +1,35 @@
+class Question {
+  final int id, answer;
+  final String question;
+  final List<String> options;
+
+  Question({this.id, this.question, this.answer, this.options});
+}
+
+const List sample_data = [
+  {
+    "id": 1,
+    "question":
+    "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "When did google release Flutter?",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "_______ is the only team to ever score 100 points in the Premier League ",
+    "options": ['Manchester City', 'Liverpool', 'Arsenal', 'Manchester United'],
+    "answer_index": 0,
+  },
+  {
+    "id": 4,
+    "question": "Which company was the first ever to be valued higher than 2 trillion US Dollars?",
+    "options": ['Microsoft', 'Apple', 'Amazon', 'Google', 'Facebook'],
+    "answer_index": 1,
+  },
+];

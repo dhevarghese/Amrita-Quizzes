06813fe2278888c92764c695a968a57cd1c4cb0b
Sai Rajaram <sairajaram.gmail.com>
2021-04-10 13:09:11 +0530

    Testing Files

---
+++ test/home_page_test.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:amrita_quizzes/app/home_page.dart';
+import 'package:amrita_quizzes/services/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'mocks.dart';
+
+void main() {
+  MyAppUser myApp;
+  MockNavigatorObserver mockNavigatorObserver;
+  StreamController<MyAppUser> onAuthStateChangedController;
+
+  setUp(() {
+    myApp = MyAppUser();
+    mockNavigatorObserver = MockNavigatorObserver();
+    onAuthStateChangedController = StreamController<MyAppUser>();
+  });
+
+  tearDown(() {
+    onAuthStateChangedController.close();
+  });
+
+
+  Future<void> pumpHomePage(WidgetTester tester) async {
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          Provider<MyAppUser>(
+            create: (_) => myApp,
+          ),
+        ],
+        child: MaterialApp(
+          home: HomePage(),
+          navigatorObservers: [mockNavigatorObserver],
+        ),
+      ),
+    );
+    // didPush is called once when the widget is first built
+    verify(mockNavigatorObserver.didPush(any, any)).called(1);
+  }
+
+  testWidgets('Sign Out Button Test', (WidgetTester tester) async {
+    await pumpHomePage(tester);
+    final SignOutButton = find.byType(FlatButton);
+    expect(SignOutButton, findsOneWidget);
+    await tester.tap(SignOutButton);
+    await tester.pumpAndSettle();
+    verify(mockNavigatorObserver.didPush(any, any)).called(1);
+  });
+}

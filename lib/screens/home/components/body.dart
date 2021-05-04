b42fb5353e1e245d3e43b7749476ed2121f2761b
HariAcidReign <haran465@gmail.com>
2021-05-04 23:20:40 +0530

    Searchbar and FuzzyLogic feat added

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,142 +1,31 @@
-import 'dart:convert';
-
+import 'package:flutter/material.dart';
 import 'package:amrita_quizzes/constants/color_constants.dart';
 import 'package:amrita_quizzes/models/Quiz_info.dart';
 import 'package:amrita_quizzes/screens/details/details_screen.dart';
-import 'package:amrita_quizzes/services/database_service.dart';
-import 'package:flutter/material.dart';
+
+import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:provider/provider.dart';
 
 import 'categorries.dart';
 import 'quiz_card.dart';
 
 class Body extends StatefulWidget {
-  Body({Key key}) : super(key: key);
+  final List products;
+  Body(this.products);
+  //Body({Key key}) : super(key: key);
 
-  _BodyState createState() => _BodyState();
+  _BodyState createState() => _BodyState(products);
 }
 
 class _BodyState extends State<Body> {
   List test = [];
-
-
-  Future<void> readJsonProducttemp() async {
-    final String response = await rootBundle.loadString('assets/QuizInfo.json');
-    final data = await json.decode(response);
-    //_items = data["Categories"];
-    setState(() {
-      test = data["QuizInfo"];
-      //print(test);
-      for (int i = 0; i < test.length; i++) {
-        var tempid = int.parse(test[i]["id"]);
-        var temptitle = test[i]["title"];
-        var tempprice = test[i]["price"];
-        var tempsize = test[i]["size"];
-        var tempimage = test[i]["image"];
-        var tempcolor = Color(int.parse(test[i]["colorcode"]));
-        //var tempdescription = test[i]["description"];
-        var tempdescription ="This quiz will assess your knowledge on the topics covered in the first four weeks of the semester. Marks will be taken into account for continuous assessment.";
-        var tempfaculty = test[i]["faculty"];
-        var tempmarks = test[i]["marks"];
-        var tempduration = test[i]["duration"];
-
-        var temp_quiz_info = Quiz_info(
-            id: tempid,
-            title: temptitle,
-            price: tempprice,
-            size: tempsize,
-            image: tempimage,
-            color: tempcolor,
-            description: tempdescription,
-            faculty: tempfaculty,
-            marks: tempmarks,
-            duration: tempduration);
-        productstest.add(temp_quiz_info);
-        products.add(temp_quiz_info);// comment this later, used for test
-
-        print(temp_quiz_info.image);
-    }
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    readJsonProducttemp();
-  }
-
-  Widget getUName(Database dbs){
-    final _formKey = GlobalKey<FormBuilderState>();
-    return Container(
-      color: Colors.white,
-      padding: new EdgeInsets.all(50),
-      child: Column(
-        children: [
-          Text(
-            'Identify yourself!',
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          FormBuilder(
-            key: _formKey,
-            child: FormBuilderTextField(name: 'uName',
-              decoration: InputDecoration(
-                labelText: 'Your name',
-                hintText: 'Ron',
-              ),
-              validator: FormBuilderValidators.required(context),
-            ),
-          ),
-          TextButton(
-            child: new Text("OK"),
-            onPressed: () {
-              final validationSuccess = _formKey.currentState.validate();
-              if(validationSuccess){
-                _formKey.currentState.save();
-                final formData = _formKey.currentState.value;
-                print(formData);
-                print(_formKey.currentState.value['uName']);
-                dbs.updateUserData(_formKey.currentState.value['uName']);
-                setState(() {});
-              }
-              //Navigator.of(context).pop();
-            },
-          )
-        ],
-      ),
-    );
+  List products;
+  _BodyState(List products) {
+    this.products = products;
   }
 
   @override
   Widget build(BuildContext context) {
-    final dbs = Provider.of<Database>(context);
-    return FutureBuilder<String>(
-        future: dbs.getUserName(),
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          print(snapshot.data);
-          if (!snapshot.hasData) {
-            // while data is loading:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            // data loaded:
-            final uName = snapshot.data;
-            if(uName == ""){
-              /*return Stack(children: [
-                quizDisplay(),
-                getUName(dbs)
-              ],);*/
-              return getUName(dbs);
-            }
-            return quizDisplay();
-          }
-        }
-    );
-  }
-
-  Widget quizDisplay(){
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -150,7 +39,6 @@ class _BodyState extends State<Body> {
                 .copyWith(fontWeight: FontWeight.bold),
           ),
         ),
-
         Categories(),
         Expanded(
           child: Padding(
@@ -161,18 +49,18 @@ class _BodyState extends State<Body> {
                   crossAxisCount: 2,
                   mainAxisSpacing: kDefaultPaddin,
                   crossAxisSpacing: kDefaultPaddin,
-                  childAspectRatio: 0.75 ,
+                  childAspectRatio: 0.75,
                 ),
                 itemBuilder: (context, index) => ItemCard(
-                  quiz_info: products[index],
-                  press: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailsScreen(
-                          quiz_info: products[index],
-                        ),
-                      )),
-                )),
+                      quiz_info: products[index],
+                      press: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsScreen(
+                              quiz_info: products[index],
+                            ),
+                          )),
+                    )),
           ),
         ),
       ],
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,20 +1,46 @@
+import 'dart:convert';
+
+import 'package:amrita_quizzes/models/Quiz_info.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:amrita_quizzes/constants/color_constants.dart';
+import 'package:amrita_quizzes/screens/home/components/body.dart';
+import 'package:amrita_quizzes/screens/home/components/qr.dart';
+//import 'package:amrita_quizzes/app/home_page.dart';
+
 //import 'package:amrita_quizzes/common_widgets/avatar.dart';
 import 'package:amrita_quizzes/common_widgets/platform_alert_dialog.dart';
 import 'package:amrita_quizzes/common_widgets/platform_exception_alert_dialog.dart';
-import 'package:amrita_quizzes/constants/color_constants.dart';
 //import 'package:amrita_quizzes/constants/keys.dart';
 import 'package:amrita_quizzes/constants/strings.dart';
-import 'package:amrita_quizzes/screens/addquiz/add_quiz_screen.dart';
-import 'package:amrita_quizzes/screens/home/components/body.dart';
-import 'package:amrita_quizzes/screens/home/components/qr.dart';
 import 'package:amrita_quizzes/services/auth_service.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_search_bar/flutter_search_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  SearchBar searchbar;
+  List test = [];
+  List products = [];
+  String searchText = "";
+  _HomeScreenState() {
+    searchbar = SearchBar(
+        buildDefaultAppBar: buildAppBar,
+        setState: setState,
+        onChanged: (value) {
+          searchText = value;
+          filter(test);
+        },
+        onSubmitted: (value) {},
+        onCleared: () {},
+        onClosed: () {},
+        inBar: false);
+  }
   Future<void> _signOut(BuildContext context) async {
     try {
       final AuthService auth = Provider.of<AuthService>(context, listen: false);
@@ -39,24 +65,101 @@ class HomeScreen extends StatelessWidget {
     }
   }
 
+  void filter(test) {
+    products.clear();
+    if (searchText == null || searchText == "") {
+      for (int i = 0; i < test.length; i++) {
+        products.add(Quiz_info(
+            id: int.parse(test[i]["id"]),
+            title: test[i]['title'],
+            price: test[i]['price'],
+            size: test[i]['size'],
+            image: test[i]['image'],
+            color: Color(int.parse(test[i]["colorcode"])),
+            description:
+                'This quiz will assess your knowledge on the topics covered in the first four weeks of the semester. Marks will be taken into account for continuous assessment.',
+            faculty: test[i]['faculty'],
+            marks: test[i]['marks'],
+            duration: test[i]['duration']));
+      }
+      setState(() {});
+      return;
+    }
+
+    for (int i = 0; i < test.length; i++) {
+      if (test[i]['title']
+          .toString()
+          .toLowerCase()
+          .contains(searchText.toLowerCase())) {
+        products.add(Quiz_info(
+            id: int.parse(test[i]["id"]),
+            title: test[i]['title'],
+            price: test[i]['price'],
+            size: test[i]['size'],
+            image: test[i]['image'],
+            color: Color(int.parse(test[i]["colorcode"])),
+            description:
+                'This quiz will assess your knowledge on the topics covered in the first four weeks of the semester. Marks will be taken into account for continuous assessment.',
+            faculty: test[i]['faculty'],
+            marks: test[i]['marks'],
+            duration: test[i]['duration']));
+      }
+    }
+    setState(() {});
+  }
+
+  Future<void> readJsonProducttemp() async {
+    final String response = await rootBundle.loadString('assets/QuizInfo.json');
+    final data = await json.decode(response);
+    //_items = data["Categories"];
+    test = data["QuizInfo"];
+    filter(test);
+    // setState(() {
+    //
+    //   //print(test);
+    //   for (int i = 0; i < test.length; i++) {
+    //     var tempid = int.parse(test[i]["id"]);
+    //     var temptitle = test[i]["title"];
+    //     var tempprice = test[i]["price"];
+    //     var tempsize = test[i]["size"];
+    //     var tempimage = test[i]["image"];
+    //     var tempcolor = Color(int.parse(test[i]["colorcode"]));
+    //     //var tempdescription = test[i]["description"];
+    //     var tempdescription ="This quiz will assess your knowledge on the topics covered in the first four weeks of the semester. Marks will be taken into account for continuous assessment.";
+    //     var tempfaculty = test[i]["faculty"];
+    //     var tempmarks = test[i]["marks"];
+    //     var tempduration = test[i]["duration"];
+    //
+    //     var temp_quiz_info = Quiz_info(
+    //         id: tempid,
+    //         title: temptitle,
+    //         price: tempprice,
+    //         size: tempsize,
+    //         image: tempimage,
+    //         color: tempcolor,
+    //         description: tempdescription,
+    //         faculty: tempfaculty,
+    //         marks: tempmarks,
+    //         duration: tempduration);
+    //     productstest.add(temp_quiz_info);
+    //     products.add(temp_quiz_info);// comment this later, used for test
+    //
+    //     print(temp_quiz_info.image);
+    // }
+    // });
+  }
+
+  @override
+  void initState() {
+    readJsonProducttemp();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(context),
-      body: Body(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        foregroundColor: Colors.white,
-        backgroundColor: Colors.lightBlueAccent,
-        elevation: 4.0,
-        onPressed: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddQuizScreen(),
-            )
-        ),
-      ),
+      appBar: searchbar.build(context),
+      body: Body(products),
     );
   }
 
@@ -88,15 +191,7 @@ class HomeScreen extends StatelessWidget {
       ),
        */
       actions: <Widget>[
-        IconButton(
-          icon: SvgPicture.asset(
-            "assets/icons/search.svg",
-            // By default our  icon color is white
-            color: kTextColor,
-          ),
-          onPressed: () {},
-        ),
-
+        searchbar.getSearchAction(context),
         Builder(
           builder: (BuildContext context) {
             return IconButton(
@@ -116,27 +211,21 @@ class HomeScreen extends StatelessWidget {
         ),
 
         //IconButton(
-          //icon: SvgPicture.asset(
-            //"assets/icons/qr.svg",
-            // By default our  icon color is white
-            //color: kTextColor,
-         // ),
-          //onPressed: () {
-           // Navigator.of(context).push(
-             // MaterialPageRoute(
-               //   builder: (BuildContext context) => Qr()),
-            //);
-          //},
+        //icon: SvgPicture.asset(
+        //"assets/icons/qr.svg",
+        // By default our  icon color is white
+        //color: kTextColor,
+        // ),
+        //onPressed: () {
+        // Navigator.of(context).push(
+        // MaterialPageRoute(
+        //   builder: (BuildContext context) => Qr()),
+        //);
+        //},
         //),
-
-
 
         SizedBox(width: kDefaultPaddin / 2)
       ],
     );
   }
 }
-
-
-
-

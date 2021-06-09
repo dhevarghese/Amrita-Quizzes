abe0012acdea933f32b8d242aa6e11b844e144a6
kishoresara <kishoresaravanan91@gmail.com>
2021-06-09 18:49:50 +0530

    cleaned code
    Signed-off-by: Kishore Saravanan <kidhoresaravanan91@gmail.com>

--- lib/screens/details/components/qr_generator.dart
+++ lib/screens/details/components/qr_generator.dart
@@ -19,14 +19,14 @@ class GenerateScreen extends StatefulWidget {
 
 class GenerateScreenState extends State<GenerateScreen> {
 
-  static const double _topSectionTopPadding = 50.0;
-  static const double _topSectionBottomPadding = 20.0;
-  static const double _topSectionHeight = 50.0;
+  //static const double _topSectionTopPadding = 50.0;
+  //static const double _topSectionBottomPadding = 20.0;
+  //static const double _topSectionHeight = 50.0;
 
   GlobalKey globalKey = new GlobalKey();
   String _dataString = "widget.displayText";
-  String _inputErrorText;
-  final TextEditingController _textController =  TextEditingController();
+  //String _inputErrorText;
+  //final TextEditingController _textController =  TextEditingController();
 
   @override
   Widget build(BuildContext context) {
